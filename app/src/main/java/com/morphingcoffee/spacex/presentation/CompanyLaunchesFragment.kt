f16e3cc4bdbf60a2edcd23527107b30558df93da
Morphing Coffee <morphingcoffee@gmail.com>
2022-02-20 23:51:16 +0000

    Added:  - Swipe Refresh launches functionality

--- app/src/main/java/com/morphingcoffee/spacex/presentation/CompanyLaunchesFragment.kt
+++ app/src/main/java/com/morphingcoffee/spacex/presentation/CompanyLaunchesFragment.kt
@@ -13,7 +13,12 @@ import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-
+/**
+ * Main screen of the app where the user sees the Company info and the list of launches.
+ *
+ * If the user selects filtering action on the app bar they will be directed to another (dialog)
+ * fragment, so the sorting & filtering is handled there.
+ **/
 class CompanyLaunchesFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
@@ -28,13 +33,20 @@ class CompanyLaunchesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
-        binding.recyclerView.adapter = adapter
+        onBindingReady()
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpObservers()
+    }
+
+    private fun onBindingReady() {
+        binding.apply {
+            recyclerView.adapter = adapter
+            swiperefresh.setOnRefreshListener { requestDataRefresh() }
+        }
     }
 
     private fun setUpObservers() {
@@ -54,6 +66,10 @@ class CompanyLaunchesFragment : Fragment() {
         }
     }
 
+    private fun requestDataRefresh() {
+        launchesViewModel.handleUserAction(LaunchesViewModel.UserAction.FullDataRefresh)
+    }
+
     private fun displayCompanyData(state: CompanyViewModel.UiState.Display) {
         val company = state.company
         binding.tv.text = getString(
@@ -70,14 +86,19 @@ class CompanyLaunchesFragment : Fragment() {
     private fun displayLaunchesData(state: LaunchesViewModel.UiState.Display) {
         // Update RecyclerView
         adapter.submitList(state.launches)
+        binding.swiperefresh.isRefreshing = false
+        // Todo make it scroll to the top?
+        //binding.recyclerView.layoutManager!!.scrollToPosition(0)
     }
 
     private fun displayProgress() {
         binding.tv.text = getString(R.string.loading)
+        binding.swiperefresh.isRefreshing = true
     }
 
     private fun displayError(errorRes: Int) {
         binding.tv.text = getString(R.string.blank)
+        binding.swiperefresh.isRefreshing = false
         Toast.makeText(requireContext(), getString(errorRes), Toast.LENGTH_SHORT).show()
     }
 
--- app/src/main/java/com/morphingcoffee/spacex/presentation/recyclerview/LaunchesAdapter.kt
+++ app/src/main/java/com/morphingcoffee/spacex/presentation/recyclerview/LaunchesAdapter.kt
@@ -2,10 +2,7 @@ package com.morphingcoffee.spacex.presentation.recyclerview
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.AsyncDifferConfig
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -13,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import coil.ImageLoader
 import coil.request.ImageRequest
 import com.morphingcoffee.spacex.R
+import com.morphingcoffee.spacex.databinding.LaunchRowItemBinding
 import com.morphingcoffee.spacex.domain.model.Launch
 import com.morphingcoffee.spacex.domain.model.LaunchStatus
 
@@ -23,50 +21,45 @@ class LaunchesAdapter(
     asyncDifferConfig: AsyncDifferConfig<Launch>
 ) : ListAdapter<Launch, LaunchesAdapter.ViewHolder>(asyncDifferConfig) {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val title: TextView = view.findViewById(R.id.title)
-        val dateAtTime: TextView = view.findViewById(R.id.dateAtTime)
-        val daysFromNow: TextView = view.findViewById(R.id.daysSinceFromNow)
-        val rocketNameType: TextView = view.findViewById(R.id.rocketNameType)
-        val icon: ImageView = view.findViewById(R.id.patchIcon)
-        val statusIcon: ImageView = view.findViewById(R.id.launchStatusIcon)
-    }
+    class ViewHolder(val binding: LaunchRowItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.launch_row_item, viewGroup, false)
-        return ViewHolder(view)
+        val binding =
+            LaunchRowItemBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = getItem(position)
         val unknownFieldString = context.getString(R.string.unknown_value_field)
 
-        // Bind data
-        holder.title.text = item.name
-        holder.dateAtTime.text = item.launchDateTime ?: unknownFieldString
-        holder.daysFromNow.text = item.launchDateTime ?: unknownFieldString
+        with(holder) {
+            // Bind data
+            binding.title.text = item.name
+            binding.dateAtTime.text = item.launchDateTime ?: unknownFieldString
+            binding.daysSinceFromNow.text = item.launchDateTime ?: unknownFieldString
 
-        // Bind rocket data field
-        val rocketName = item?.rocket?.name ?: unknownFieldString
-        val rocketType = item?.rocket?.type ?: unknownFieldString
-        val rocketDescription =
-            context.getString(R.string.launch_rocket_field_template, rocketName, rocketType)
-        holder.rocketNameType.text = rocketDescription
+            // Bind rocket data field
+            val rocketName = item?.rocket?.name ?: unknownFieldString
+            val rocketType = item?.rocket?.type ?: unknownFieldString
+            val rocketDescription =
+                context.getString(R.string.launch_rocket_field_template, rocketName, rocketType)
+            binding.rocketNameType.text = rocketDescription
 
-        // Bind launch status icon
-        when (item.launchStatus) {
-            LaunchStatus.Successful -> holder.statusIcon.setBackgroundResource(R.drawable.done)
-            LaunchStatus.Failed -> holder.statusIcon.setBackgroundResource(R.drawable.close)
-            LaunchStatus.FutureLaunch -> holder.statusIcon.setBackgroundResource(R.drawable.question_mark)
+            // Bind launch status icon
+            when (item.launchStatus) {
+                LaunchStatus.Successful -> binding.launchStatusIcon.setBackgroundResource(R.drawable.done)
+                LaunchStatus.Failed -> binding.launchStatusIcon.setBackgroundResource(R.drawable.close)
+                LaunchStatus.FutureLaunch -> binding.launchStatusIcon.setBackgroundResource(R.drawable.question_mark)
+            }
+
+            // Bind patch image
+            val patchImage = item.links?.patchImage
+            val r = imageRequestBuilder
+                .data(patchImage?.smallURL ?: patchImage?.largeURL)
+                .target(binding.patchIcon)
+            imageLoader.enqueue(r.build())
         }
-
-        // Bind patch image
-        val patchImage = item.links?.patchImage
-        val r = imageRequestBuilder
-            .data(patchImage?.smallURL ?: patchImage?.largeURL)
-            .target(holder.icon)
-        imageLoader.enqueue(r.build())
     }
 }
 
